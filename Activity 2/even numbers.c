bc67228a89e5adccdbc26a49b099efebfeab52f0
Jodieorantia <2025-1-00401@lpu.edu.ph>
2025-10-24 08:23:21 +0800

    Update even numbers.c

--- Activity 2/even numbers.c
+++ Activity 2/even numbers.c
@@ -2,12 +2,12 @@
 
 int main() {
     int i;
-    for(i = 0; i <= 10; i++) {
-        if(i % 2 == 0) {
-            printf("%d\n", i);
-        }
     
+    for(i = 0; i <= 10; i+=2 ) {
+        printf("%d\n", i);
+        
     }
+    
     return 0;
 }
 
