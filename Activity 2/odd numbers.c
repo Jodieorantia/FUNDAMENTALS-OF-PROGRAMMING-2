7b8c96eb1f91c675230affc79ce397a80bee5cbd
Jodieorantia <2025-1-00401@lpu.edu.ph>
2025-10-24 08:26:48 +0800

    Update odd numbers.c

--- Activity 2/odd numbers.c
+++ Activity 2/odd numbers.c
@@ -2,11 +2,10 @@
 
 int main() {
     int i;
-    for(i = 0; i <= 10; i++) {
-        if(i % 2 != 0) {
-            printf("%d\n", i);
-        }
+    for(i = 0; i <= 10; i+=2) {
+        printf("%d\n", i);
     }
     return 0;
 }
+
 
